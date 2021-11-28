269d64b32d3b595080a5dc7b7b6c32930c94a824
MinsuHan <trms7794han@gmail.com>
2021-11-28 16:04:25 +0900

    modified board_screen: load Posts

--- lib/screens/board_screen.dart
+++ lib/screens/board_screen.dart
@@ -9,6 +9,10 @@ class BoardScreen extends StatelessWidget {
   static const routeName = './board';
   const BoardScreen({Key? key}) : super(key: key);
 
+  Future<void> _refreshPosts(BuildContext context, String boardId) async {
+    await Provider.of<Posts>(context, listen: false).fetchAndSetPosts(boardId);
+  }
+
   @override
   Widget build(BuildContext context) {
     final args =
@@ -16,13 +20,43 @@ class BoardScreen extends StatelessWidget {
     final boardId = args.boardId;
     final boardName = args.boardName;
 
-    final loadedPosts = Provider.of<Posts>(context, listen: false);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('$boardName'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditPostScreen.routeName);
+            },
+          ),
+        ],
       ),
-      // body:
+      body: FutureBuilder(
+        future: _refreshPosts(context, boardId),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : RefreshIndicator(
+                    onRefresh: () => _refreshPosts(context, boardId), 
+                    child: Consumer<Posts>(
+                      builder: (ctx, postsData, _) => Padding(
+                        padding: EdgeInsets.all(8),
+                        child: ListView.builder(
+                          itemCount: postsData.items.length,
+                          itemBuilder: (_, i) => Column(children: [
+                            PostItem(
+                              
+                            )
+                          ],),
+                        ),
+                        ),
+                      ),
+                    ),
+                  ),
+      );
     );
   }
 }
