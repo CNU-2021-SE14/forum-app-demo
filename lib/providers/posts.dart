bd3165073f62fde8d30776ec5d5bd3a715f53337
MinsuHan <trms7794han@gmail.com>
2021-11-28 15:14:36 +0900

    implemented addPost() in posts.dart

--- lib/providers/posts.dart
+++ lib/providers/posts.dart
@@ -20,7 +20,40 @@ class Posts with ChangeNotifier {
 
   Future<void> fetchAndSetPosts(String boardId) async {}
 
-  Future<void> addPost(Post post) async {}
+  Future<void> addPost(Post post) async {
+    final url = Uri.https('flutterforumdemoapp-default-rtdb.firebaseio.com',
+        '/posts.json?auth=$authToken');
+    final timeStamp = DateTime.now();
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'title': post.title,
+          'contents': post.contents,
+          'datetime': timeStamp.toIso8601String(),
+          'boardId': post.boardId,
+          'creatorId': post.userId,
+        }),
+      );
+
+      final newPost = Post(
+        title: post.title,
+        contents: post.contents,
+        datetime: timeStamp,
+        boardId: post.boardId,
+        userId: post.userId,
+        id: json.decode(response.body)['name'],
+      );
+
+      _items.add(newPost);
+
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw (error);
+    }
+  }
 
   Future<void> updatePost(String id, Post newPost) async {}
 
